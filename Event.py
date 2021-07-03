551f55e92837fb8dff2d265942c1ed5015afd215
alirezaisavand <isavand.alireza@gmail.com>
2021-07-03 19:57:26 +0430

    creating Event.py

--- Event.py
+++ Event.py
@@ -11,4 +11,8 @@ class Reception(Event):
     def handle_event(self):
 
 class End_service(Event):
+    def __init__(self, part, server, time):
+        self.part = part
+        self.server = server
+        Event.__init__(self, time=time)
     def handle_event(self):
