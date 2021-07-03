a78a293546535fe7ecaa262d82d211cfa656c42f
Mohammad <m.mohammadi1378@outlook.com>
2021-07-03 21:02:29 +0430

    add Simulator.py

---
+++ Simulator.py
@@ -0,0 +1,8 @@
+import Department
+
+class Simulator:
+    def __init__(self, N, lam, alpha, MUs):
+        self.Departments = []
+        for i in range(N):
+            self.Departments.add(Department.Department())#todo
+
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello simulation")
+
+if __name__ == '__main__':
+    N, lam, mu, alpha = map(int, input().split())
+    MUs = []
+    for i in range(N):
+        MUs.append(list(map(int, input())))
+
+    
