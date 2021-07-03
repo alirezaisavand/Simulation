d482cba29f49080b51679a2683bffbbd8bee58ba
Mohammad <m.mohammadi1378@outlook.com>
2021-07-03 20:54:43 +0430

    add git ignore

--- Department.py
+++ Department.py
@@ -1,4 +1,16 @@
 class Department():
-    def __init__(self, servers):
-        self.servers = servers
-        self.queue = []
+    def __init__(self, MUs, max_priority):
+        self.servers = []
+        self.max_priority = max_priority
+        self.queues = []
+        self.customers_in_queue = 0
+        self.lengths_of_queue = []
+
+    def add(self, event):
+        pass
+
+    def depart(self):
+        pass
+
+    def modify_avg(self):
+        pass
--- Server.py
+++ Server.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 class Server():
-    def __init__(self, miu):
-        self.miu = miu
+    def __init__(self, mu):
+        self.mu = mu
         self.available = True
+
     def get_service_time(self):
         return int(np.random.exponential(self.miu))
